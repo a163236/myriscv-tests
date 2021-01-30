9940ed687d65dbfa983094befa4e745c7d4c8f38
a163236 <nagakeita12@gmail.com>
2021-01-30 11:01:22 +0900

    commi

--- src/sample3.c
+++ src/sample3.c
@@ -1,17 +1,17 @@
 int main(void){
 
-    int a[2];
+    int a[10],b[10], c[10];
 
-    for (int i = 0; i < 2; i++)
-    {
-        a[i]=i;
-    }
-/*    
     for (int i = 0; i < 10; i++)
     {
-        c += a[i] + b[i]; 
+        a[i]=i; b[i]=i;
     }
- */  
-    return a[1];
+    
+    for (int i = 0; i < 10; i++)
+    {
+        c[i] += a[i] + b[i]; 
+    }
+     
+    return c[1];
 
 }
